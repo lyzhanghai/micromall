5feb9d348707aa05c574fd140093e354c84744df
zhangzxiang91@gmail.com <zhangzxiang91@gmail.com>
2016-05-18 22:58:43 +0800

    commit

--- src/main/java/com/micromall/web/controller/MyOrdersController.java
+++ src/main/java/com/micromall/web/controller/MyOrdersController.java
@@ -1,5 +1,6 @@
 package com.micromall.web.controller;
 
+import com.google.common.collect.Maps;
 import com.micromall.service.OrderService;
 import com.micromall.web.resp.ResponseEntity;
 import com.micromall.web.security.annotation.Authentication;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Map;
 
 /**
  * Created by zhangzx on 16/3/21.
@@ -22,7 +24,25 @@ public class MyOrdersController extends BasisController {
 	private OrderService orderService;
 
 	/**
-	 * 我的订单列表（包含各种状态的订单）[待付款、待发货、待收货、已完成、退货/取消]
+	 * 统计
+	 *
+	 * @return
+	 */
+	@RequestMapping(value = "/statistics")
+	public ResponseEntity<?> statistics() {
+		Map<String, Integer> data = Maps.newHashMap();
+		data.put("waitPay", 2);//未付款订单
+		data.put("waitDelivery", 4);//待发货订单
+		data.put("waitReceive", 2);//待收货订单
+		data.put("complete", 8);//已完成订单
+		data.put("refund_closed", 9);//已取消订单
+
+		// return ResponseEntity.Success(orderService.statistics(getLoginUser().getUid()));
+		return ResponseEntity.Success(data);
+	}
+
+	/**
+	 * 我的订单列表（包含各种状态的订单）[0:待付款、1:待发货、2:待收货、3:已完成、4:退货/取消]
 	 *
 	 * @param status 订单状态
 	 * @return
