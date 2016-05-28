abb314dbae91354a2c388d029ad35b7ce08163d0
zhangzxiang91@gmail.com <zhangzxiang91@gmail.com>
2016-05-28 17:01:18 +0800

    commit

--- src/main/java/com/micromall/web/security/FrontAuthenticationInterceptor.java
+++ src/main/java/com/micromall/web/security/FrontAuthenticationInterceptor.java
@@ -119,12 +119,10 @@ public class FrontAuthenticationInterceptor extends AbstractBaseInterceptor {
 
 	private LoginUser _getLoginUser(HttpServletRequest request) {
 		LoginUser loginUser = (LoginUser)request.getSession().getAttribute(CommonEnvConstants.LOGIN_SESSION_KEY);
-
-		//		boolean debugAuth = CommonEnvConstants.ENV.isDevEnv() && !request.getParameterMap().containsKey(
-		//				"testAuth")/* && request.getParameterMap().containsKey("debugAuth")*/;
-		//		if (debugAuth) {
-		//			loginUser = _MockLogin(request);
-		//		}
+		boolean debugAuth = loginUser == null && CommonEnvConstants.ENV.isDevEnv() && request.getParameterMap().containsKey("debugAuth");
+		if (debugAuth) {
+			loginUser = _MockLogin(request);
+		}
 		return loginUser;
 	}
 
