b736df9f2ed75c50fac8af58105476af20f26ab2
陈明康 <mingkang1993@qq.com>
2016-05-29 13:30:41 +0800

    修改

--- src/main/webapp/resSatic/js/run/httpResponse.js
+++ src/main/webapp/resSatic/js/run/httpResponse.js
@@ -1,11 +1,26 @@
 /**
  * Created by chenmingkang on 16/3/8.
  */
-app.factory('authHttpResponseInterceptor',['$q','$location','$rootScope','$timeout',function($q,$location,$rootScope,$timeout){
+app.factory('authHttpResponseInterceptor',['$q','$location','$rootScope','$injector',function($q,$location,$rootScope,$injector){
+    var responseLoad = false;
+
+    function resFn(){
+        responseLoad = true;
+        $rootScope.loading = false;
+    }
+
     return {
         request: function (config) {
+            var config = config || {};
+            config.timeout = 10000;
+            config.load = config.load || true;
+            responseLoad = false;
+
             if(config.url.indexOf('.html') > -1 || config.url.indexOf('json') > -1){
                 return config;
+            }
+            if (config.load && !responseLoad){
+                $rootScope.loading = true;
             }
             if(config.method == 'GET'){
                 config.params = config.params || {};
@@ -19,19 +34,27 @@ app.factory('authHttpResponseInterceptor',['$q','$location','$rootScope','$timeo
             return config;
         },
         response: function(response){
+            var messageFn = $injector.get('messageFactory');
             if (typeof response.data === 'object' && response.status != 200) {
                 switch (response.data.code){
                     case '-1':
-                        messageFactory({text:'请登录'});
+                        messageFn({text:'请登录'});
                         location.href = response.data.data;
                         break;
                     case '1':
-                        messageFactory({text: response.data.msg});
+                        messageFn({text: response.data.msg});
                 }
-                $rootScope.loading = false;
+                resFn();
                 return $q.reject(response);
             }
+            resFn();
             return response || $q.when(response);
+        },
+        responseError: function(err){
+            var messageFn = $injector.get('messageFactory');
+            resFn();
+            messageFn({text : '服务器出错请求出错'});
+            return $q.reject(err);
         }
     }
 }]);
